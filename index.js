6c7ecc792c40464b48418ea522c00e38ba91b58d
Quang Thanh <59902482+Thanhnq1608@users.noreply.github.com>
2021-03-27 10:42:39 +0700

    thanh

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
-var http=require('http');
-var url=require('url');
-var cal =require('./Cal');
-
-http.createServer(function (request,response){
-
-  response.writeHead(200,{'Content-Type':'text/html'});
-  var tong=cal.tinhTong(3,5);
-  response.end('Hello everrybody say sadasdsadws------'+tong);
-}).listen(8082);
+var http = require("http");
+var fs = require("fs");
+http.createServer(function(request, reponse) {
+  reponse.writeHead("200", { "Content-Type": "text/html" });
+  var url = request.url;
+  if (url == "/") {
+    fs.readFile("index.html", function(error, data) {
+      if (error == null) {
+        reponse.write(data);
+        reponse.end();
+      } else {
+        reponse.end(error);
+      }
+    });
+  } else if (url == "/insert") {
+    fs.writeFile("test.txt", "\n ghi vao file thu xem insert", function(error) {
+      if (error == null) {
+        reponse.end("ghi thanh cong");
+      } else {
+        reponse.end(error);
+      }
+    });
+  } else if (url == "/append") {
+    fs.appendFile("test.txt", "\n ghi vao file thu xem append", function(error) {
+      if (error == null) {
+        reponse.end("Append thanh cong");
+      } else {
+        reponse.end(error);
+      }
+    });
+  } else if (url == "/unlink") {
+    fs.unlink("test.txt", function(error) {
+      if (error == null) {
+        reponse.end("Xoa thanh cong");
+      } else {
+        reponse.end(error);
+      }
+    });
+  } else if (url == "/rename") {
+    fs.rename("test.txt", "test2.txt", function(error) {
+      if (error == null) {
+        reponse.end("Rename thanh cong");
+      } else {
+        reponse.end(error);
+      }
+    });
+  } else {
+    reponse.end("404 not found")
+  }
+}).listen(process.env.PORT || '7777');
